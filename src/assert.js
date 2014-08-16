df6f31bd6c224fe98657a1b1a017a92ea67fed2b
gcanti <giulio.canti@gmail.com>
2014-08-16 11:38:07 +0200

    fix print renamed

--- src/assert.js
+++ src/assert.js
@@ -9,7 +9,7 @@ function fail(message) {
 function assert(guard) {
   if (guard !== true) {
     var args = slice.call(arguments, 1);
-    var message = args[0] ? print.apply(null, args) : 'assert(): failed';
+    var message = args[0] ? format.apply(null, args) : 'assert(): failed';
     fail(message); 
   }
 }
