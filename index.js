93fb6c1ff86604c83b4dbe5e31813d507cf96267
gcanti <giulio.canti@gmail.com>
2015-01-13 10:51:42 +0100

    optimized assert

--- index.js
+++ index.js
@@ -29,10 +29,9 @@
     options.onFail(message);
   }
 
-  function assert(guard) {
+  function assert(guard, message) {
     if (guard !== true) {
-      var args = slice.call(arguments, 1);
-      var message = args[0] ? format.apply(null, args) : 'assert failed';
+      message = message ? format.apply(null, slice.call(arguments, 1)) : 'assert failed';
       fail(message);
     }
   }
