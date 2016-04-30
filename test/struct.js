5f2236425096245b3710d5e01eae77cbb0f3794f
gcanti <giulio.canti@gmail.com>
2016-04-30 19:10:11 +0200

    struct's extend accepts also interfaces

--- test/struct.js
+++ test/struct.js
@@ -113,18 +113,22 @@ describe('t.struct(props, [name])', function () {
       assert.deepEqual(NewType.meta.props.c, t.Boolean);
     });
 
-    it('should handle a struct (or list of structs) as argument', function () {
+    it('should handle an array of objects or structs or interfaces as argument', function () {
       var A = t.struct({a: t.String}, 'A');
       var B = t.struct({b: t.String}, 'B');
       var C = t.struct({c: t.String}, 'C');
       var MixinD = {d: t.String};
-      var E = A.extend([B, C, MixinD]);
+      var I = t.inter({i: t.String}, 'I');
+      I.prototype.imethod = function() {};
+      var E = A.extend([B, C, MixinD, I]);
       assert.deepEqual(E.meta.props, {
         a: t.String,
         b: t.String,
         c: t.String,
-        d: t.String
+        d: t.String,
+        i: t.String
       });
+      assert.equal(E.prototype.imethod === I.prototype.imethod, true);
     });
 
     it('should support prototypal inheritance', function () {
