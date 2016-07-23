29020dd6bbedff591f79633a1b2ae36113bb15bd
Giulio Canti <giulio.canti@gmail.com>
2016-07-23 09:18:50 +0200

    change shallow copy in order to improve perfs (#234)

--- perf/perf.js
+++ perf/perf.js
@@ -16,63 +16,20 @@ function onComplete() {
   });
 }
 
-//
-// vanilla
-//
+var map = {};
 
-function ClassType(name) {
-  this.name = name;
+for (var i = 0; i < 10 ; i++) {
+  map['a' + i] = i;
 }
 
-var classInput = 'giulio';
+var spec = { a1: { $set: 2 } };
 
-function ClassVersion() {
-  new ClassType(classInput);
-}
-
-//
-// t.struct
-//
-
-var structInput = {name: 'giulio'};
-
-var StructType = t.struct({
-  name: t.String
-});
-
-function StructVersion() {
-  new StructType(structInput);
+function updateTest() {
+  var obj = t.update(map, spec);
+  obj.a1;
 }
 
 Benchmark.Suite({ })
-  .add('ClassVersion', ClassVersion)
-  .add('StructVersion', StructVersion)
+  .add('updateTest', updateTest)
   .on('complete', onComplete)
   .run({ async: true });
-
-/*
-
-  ======================
-  Results (node v0.12.2)
-  ======================
-
-  (development)
-  var t = require('../');
-  ClassVersion 96726638.34147379 ops/sec
-  StructVersion 173322.08316550445 ops/sec
-
-  ---
-
-  (development build)
-  var t = require('../dist/tcomb');
-  ClassVersion 97459116.74105316 ops/sec
-  StructVersion 547549.8587466488 ops/sec
-
-  ---
-
-  (production build) `Object.freeze` calls and asserts stripped out
-  var t = require('../dist/tcomb.min');
-  ClassVersion 98850069.98176897 ops/sec
-  StructVersion 2272302.152337918 ops/sec
-
-*/
